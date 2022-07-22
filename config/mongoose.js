39eaa0fa64140efa33948af80a7d37759d21b544
irshad <irshadali0729@gmail.com>
2022-07-22 20:25:07 +0530

    connect to mongo atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,16 +1,29 @@
+// const dotenv = require("../dotenv").config();
 // Require moongoose library
+const express = require("express");
+
 const mongoose = require("mongoose");
 
+const DB =
+  "mongodb+srv://irshad:root@cluster0.l9zls.mongodb.net/bugtrackdb?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Mongo Atlas server connected ");
+  })
+  .catch((err) => {
+    console.log("error", err);
+  });
 // Established the connection to database
-mongoose.connect("mongodb://localhost/issue_track_db");
+// mongoose.connect("mongodb://localhost/issue_track_db");
 
+// const db = mongoose.connection;
 
-const db = mongoose.connection;
+// // display message upon error in connecting db
+// db.on("error", console.error.bind(console, "error in connecting to db"));
 
-// display message upon error in connecting db
-db.on("error", console.error.bind(console, "error in connecting to db"));
-
-// display message upon successfull connection
-db.once("open", function () {
-  console.log("Successfully connected to the database");
-});
+// // display message upon successfull connection
+// db.once("open", function () {
+//   console.log("Successfully connected to the database");
+// });
